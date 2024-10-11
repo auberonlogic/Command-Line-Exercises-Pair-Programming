c95e3fed65f53dcb9966048c5356a7fc570ad661
ryan theerman <ryantheerman@proton.me>
2024-10-11 11:46:48 -0400

    finished pair programming exercises

--- src/main/java/com/techelevator/DecimalToBinary.java
+++ src/main/java/com/techelevator/DecimalToBinary.java
@@ -1,36 +1,37 @@
 package com.techelevator;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class DecimalToBinary {
 
 	public static void main(String[] args) {
 
-//		Scanner scan = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
 
-//		System.out.print("Please enter in a series of decimal values (separated by spaces): ");
-//		String decimals = scan.nextLine();
-//		String[] decimalArray = decimals.split(" ");
+		// prompt user for input
+		System.out.print("Please enter in a series of decimal values (separated by spaces): ");
+		String decimals = scan.nextLine();
+		String[] decimalArray = decimals.split(" ");
 
-//		for (int i = 0; i < decimalArray.length; i++) {
-//			int num = Integer.parseInt(decimalArray[i]);
+		// iterate over each element of user input
+		for (int i = 0; i < decimalArray.length; i++) {
+			int num = Integer.parseInt(decimalArray[i]);
 
-			// how do I convert a decimal to binary?
-			// divide the number by 2 and note the remainder as the least significant bit
-			// divide the quotient by 2 and note the remainder as the next significant bit
-			// repeat until the quotient is 0 and note the remainder as the most significant bit.
+			// instantiate a list of Integers to hold the bits in reverse order
+			List<Integer> bits = new ArrayList<>();
 
+			System.out.print("\n" + num + " in binary is ");
 
-//		}
+			// determine the bits and add them to the list
+			for (int j = num; num > 0; num /= 2) {
+				int bit = num % 2;
+				bits.add(bit);
+			}
 
-		int num;
-		for (num = 254; num > 0; num /= 2) {
- 			int bit = num % 2;
-			System.out.println(bit);
+			// print the bits in the correct order
+			for (int k = bits.size() - 1; k >= 0; k--) {
+				System.out.print(bits.get(k));
+			}
 		}
-
 	}
-
 }
